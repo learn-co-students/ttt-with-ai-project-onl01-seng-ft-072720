98b8a3a441850a19c8aeae87fa637ba9acfb84e2
DavidFuents <Davidf0827@gmail.com>
2020-08-17 20:55:31 -0400

    Done.

--- lib/board.rb
+++ lib/board.rb
@@ -1,46 +1,71 @@
 class Board
-    attr_accessor :cells
-  
-    @cells = []
-  
-    def reset!
-         self.cells = Array.new(9, " ")
-    end
-  
-    def initialize
-        self.cells = Array.new(9, " ")
-    end
-  
-    def display
-        puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
+
+	attr_accessor :cells
+
+	@cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+	def initialize
+		self.reset!
+	end
+
+	def reset!
+		@cells = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+	end
+
+	def display
+		puts " #{@cells[0]} | #{@cells[1]} | #{@cells[2]} "
+		puts "-----------"
+        puts " #{@cells[3]} | #{@cells[4]} | #{@cells[5]} "
         puts "-----------"
-        puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
-        puts "-----------"
-        puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
-    end
-  
-    def position(user_input)
-        self.cells[user_input.to_i-1]
-    end
-  
-    def full?
-        self.cells.all? {|cell| cell == "X" || cell == "O"}
-    end
-  
+        puts " #{@cells[6]} | #{@cells[7]} | #{@cells[8]} "
+	end
+
+	def position(input)
+		input = input_to_index(input)
+		@cells[input.to_i]
+	end
+
+	def input_to_index(user_input)
+		user_input.to_i - 1
+	end
+
+	 def full?
+     if @cells.detect{|i| i == "" || i == " "}
+       return false
+     else
+       return true
+     end
+   end
+
     def turn_count
-        self.cells.count("X") + self.cells.count("O")
-    end
-  
-    def taken?(position) 
-        self.cells[position.to_i-1] == "X" || self.cells[position.to_i-1] == "O"
-    end
-  
-    def valid_move?(position)
-        !taken?(position) && position.to_i >0 && position.to_i <=9
-    end
-  
-    def update(position, player)
-        self.cells[position.to_i-1] = player.token
-    end
-  
-end 
+     num_turns = 0
+     @cells.each do |space|
+       if space == "X" || space == "O"
+         num_turns += 1
+       end
+     end
+     return num_turns
+   end
+
+  	def taken?(loc)
+  	 loc = input_to_index(loc.to_i)
+     if(@cells[loc] == "X" || @cells[loc] == "O")
+     	true
+     else
+     	false
+     end
+   end
+
+   def valid_move?(move)
+   	 move.to_i.between?(1, 9) && !taken?(move)
+   end
+
+   def update(move, player)
+   		if(valid_move?(move))
+   			move = input_to_index(move)
+   			@cells[move] = player.token
+   			@cells
+   		end
+   end
+
+end
--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,10 +1,9 @@
 module Players
-    class Human < Player
-                   
-      def move(board)
-        puts "Please enter 1-9:"
-        gets.strip
-      end
-    end
-
-end 
+	class Human < Player
+		def move(board)
+			puts "Please enter 1-9:"
+     		input = gets.strip
+     		input	
+		end
+	end
+end
